03b88aae0216d7994ecb24735a7f90989aba7b05
danya-gharbieh <danya.gh.91@gmail.com>
2024-03-11 14:56:42 +0100

    adding front-end

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const { report } = require('node:process');
+const path = require('node:path');
+
+//report.writeReport();
+
+const createWindow = () => {
+    const mainWindow =  new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    })
+
+    mainWindow.loadFile('index.html');
+    // Open the DevTools.
+    //mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    // if not macOS
+    if (process.platform !== 'darwin') app.quit()
+})
+
---
+++ renderer.js
@@ -0,0 +1,11 @@
+const { bootstrap } = require('./bower_components/bootstrap/dist/js/bootstrap.bundle.js');
+
+var triggerTabList = [].slice.call(document.querySelectorAll('#list-tab a'))
+triggerTabList.forEach(function (triggerEl) {
+    var tabTrigger = new bootstrap.Tab(triggerEl)
+
+    triggerEl.addEventListener('click', function (event) {
+        event.preventDefault()
+        tabTrigger.show()
+    })
+})
