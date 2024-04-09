54f7eb7576e55723ea2d799277bf098561746e14
danya-gharbieh <danya.gh.91@gmail.com>
2024-04-09 12:53:52 +0200

    change variable definition

--- js/spine.js
+++ js/spine.js
@@ -1,4 +1,4 @@
-var pageDetails={
+let pageDetails={
   'cover': { 
     'text': {
       'EN': "Little Red Riding Hood",
@@ -51,8 +51,8 @@ var pageDetails={
 
 // page orgnisation down arrow icon event 
 $(document).on('click', '#pageList .bi-arrow-down', function(e) {
-  var cItem = $(this).closest('a');
-  var tItem = cItem.next('a');
+  let cItem = $(this).closest('a');
+  let tItem = cItem.next('a');
   if (tItem.length == 0 || tItem.attr('id') == 'plusIcon' || tItem.attr('id') == 'credit'){
     //[1] to insert the item after the cover page
     cItem.insertBefore($(this).closest('div').children()[1]);
@@ -63,10 +63,10 @@ $(document).on('click', '#pageList .bi-arrow-down', function(e) {
 
 // page orgnisation up arrow icon event 
 $(document).on('click', '#pageList .bi-arrow-up', function(e) {
-  var cItem = $(this).closest('a');
-  var tItem = cItem.prev('a');
+  let cItem = $(this).closest('a');
+  let tItem = cItem.prev('a');
   if (tItem.length == 0 || tItem.attr('id') == 'cover'){
-    var childrenLength = $(this).closest('div').children().length
+    let childrenLength = $(this).closest('div').children().length
     //it should be -1 but -3 because of the plus list item and the credit page
     cItem.insertAfter($(this).closest('div').children()[childrenLength - 3]);
     return
@@ -128,15 +128,15 @@ $(document).on('click', '.otherFiles', function(e) {
 
 //Add new page anchor element 
 function createPage(){
-  var pageLength = 0;
-  var pages = document.getElementById('pageList');
-  var creditPage = document.getElementById('credit');
-  var aElem = document.createElement('a');
+  let pageLength = 0;
+  let pages = document.getElementById('pageList');
+  let creditPage = document.getElementById('credit');
+  let aElem = document.createElement('a');
   pageLength = $('#pageList a').length - 2;
   aElem.setAttribute('href', '#');
   aElem.setAttribute('id', pageLength);
   aElem.setAttribute('class', 'list-group-item list-group-item-action');
-  var elemText= document.createTextNode('Page '+pageLength);
+  let elemText= document.createTextNode('Page '+pageLength);
   aElem.appendChild(elemText);
   //Create up and down icons to the element
   createIcon(aElem, 'bi bi-arrow-up icons','Move the page up');
@@ -148,17 +148,17 @@ function createPage(){
 
 //fill fields with the page details 
 function fillData(){
-  var page = $("#pageList .list-group-item.active");
-  var pageId = $("#pageList .list-group-item.active").attr('id');
-  var pageLabel = document.getElementById('contentBoxLabel');
+  let page = $("#pageList .list-group-item.active");
+  let pageId = $("#pageList .list-group-item.active").attr('id');
+  let pageLabel = document.getElementById('contentBoxLabel');
   $('#contentBoxLabel').text(page.text());
   if (pageId !='cover' && pageId !='credit'){
     createIcon(pageLabel, 'bi bi-trash3-fill icons','delete page', 'deletePage')
   }
   $('#contentBox table').each(function(){
-    var table =  $(this);
-    var theadTxt = $(this).children('thead').children('tr').children('th');
-    var section = theadTxt.attr('name');
+    let table =  $(this);
+    let theadTxt = $(this).children('thead').children('tr').children('th');
+    let section = theadTxt.attr('name');
     $(this).find('tbody').html('');
     createTableBody(table, pageId, section);
   })
@@ -166,7 +166,7 @@ function fillData(){
 
 //create body of the table based on the filled data
 function createTableBody(tbl, pageId, section){
-  var tbdy = document.createElement('tbody');
+  let tbdy = document.createElement('tbody');
   if(section =='text' || section =='alt' || section =='narration'){
     createLangRows(tbl, tbdy, pageId, section);
     return;
@@ -175,17 +175,17 @@ function createTableBody(tbl, pageId, section){
     newImagesScripts(tbl, tbdy)
     return;
   }
-  for(var val in pageDetails[pageId][section]) { 
-    var tr = document.createElement('tr');
-    var th = document.createElement('th');
+  for(let val in pageDetails[pageId][section]) { 
+    let tr = document.createElement('tr');
+    let th = document.createElement('th');
     th.appendChild(document.createTextNode(val));
     th.setAttribute('scope','row');
     th.setAttribute('contenteditable','false');
     tr.appendChild(th);
-    var td = document.createElement('td');
+    let td = document.createElement('td');
     td.appendChild(document.createTextNode(pageDetails[pageId][section][val]));
     if (section =='imagesScripts'){
-      var browseBtn = document.createElement('button');
+      let browseBtn = document.createElement('button');
       browseBtn.setAttribute('type', 'button');
       browseBtn.setAttribute('alt', 'Browse images and scripts button');
       if(pageId =='cover' && val =='Image'){
@@ -212,17 +212,17 @@ function createLangRows(tbl, tbdy, pageId, section){
   if(pageId =='credit' && (section =='narration' || section =='alt')){
     return;
   }
-  var value = '';
-  var langs = JSON.parse(sessionStorage.getItem("pubLang"));
+  let value = '';
+  let langs = JSON.parse(sessionStorage.getItem("pubLang"));
   for (let i = 0; i < langs.length; i++) {
-    var tr = document.createElement('tr');
-    var th = document.createElement('th');
+    let tr = document.createElement('tr');
+    let th = document.createElement('th');
     th.appendChild(document.createTextNode(langs[i]));
     th.setAttribute('scope','row');
     th.setAttribute('class','langHeader');
     th.setAttribute('contenteditable','false');
     tr.appendChild(th);
-    var td = document.createElement('td');
+    let td = document.createElement('td');
     if (pageDetails[pageId] && pageDetails[pageId][section]){
       value = pageDetails[pageId][section][langs[i]];
     }
@@ -232,7 +232,7 @@ function createLangRows(tbl, tbdy, pageId, section){
       td.appendChild(document.createTextNode(''));
     }
     if (section =='narration') {
-      var browseBtn = document.createElement('button');
+      let browseBtn = document.createElement('button');
       browseBtn.setAttribute('type', 'button');
       browseBtn.setAttribute('alt', 'Browse narration button');
       browseBtn.setAttribute('class', 'btn btn-secondary browseBtn narrations');
@@ -248,14 +248,14 @@ function createLangRows(tbl, tbdy, pageId, section){
 
 //create images and scripts panel for new pages 
 function newImagesScripts(tbl, tbdy){
-  var tr = document.createElement('tr');
-  var th = document.createElement('th');
+  let tr = document.createElement('tr');
+  let th = document.createElement('th');
   th.appendChild(document.createTextNode('Image'));
   th.setAttribute('scope','row');
   th.setAttribute('contenteditable','false');
   tr.appendChild(th);
-  var td = document.createElement('td');
-  var browseBtn = document.createElement('button');
+  let td = document.createElement('td');
+  let browseBtn = document.createElement('button');
   browseBtn.setAttribute('type', 'button');
   browseBtn.setAttribute('alt', 'Browse images and scripts button');
   browseBtn.setAttribute('class', 'btn btn-secondary browseBtn');
