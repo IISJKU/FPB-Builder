5d33cb1e880e37d3e0df3431ad6f2947ba01eab4
danya-gharbieh <danya.gh.91@gmail.com>
2024-05-02 09:04:03 +0200

    updates

--- js/metadata.js
+++ js/metadata.js
@@ -201,7 +201,7 @@ function createTable(tableTitle, elemID, itemVal, langChange) {
 function langMetaAttr(tbl, tbdy, itIntVal, elemId) {
   let bookDetObj = parseSessionData("bookDetails");
   let value = "";
-  let langs = JSON.parse(sessionStorage.getItem("pubLang"));
+  let langs = parseSessionData("pubLang");
   for (let i = 0; i < langs.length; i++) {
     let tr = document.createElement("tr");
     let th = document.createElement("th");
